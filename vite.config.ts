8ef7873ef4a6552cd4fe1600447f17862eb2121b
Taekwon Yoo <takun617@gmail.com>
2026-01-29 18:43:31 +0900

    chore: pwa 지원

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'My App',
+      short_name: 'App',
+      description: 'My PWA App',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      // Workbox options
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    }
+  })],
   base: "tiger-sound-pad",
   server: {
     port: 3000,
